9658543ceaaf661f5dd1a0d651cc9fe7cab89ac3
Max <maxim.macari.dam@gmail.com>
2020-09-28 13:13:19 +0200

    Initial Commit

---
+++ Clock_SwuiftUI/Clock_SwuiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_SwuiftUIApp.swift
+//  Clock_SwuiftUI
+//
+//  Created by Maxim Macari on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Clock_SwuiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
